f933b881e334dd126ff1d2d53f1b065cb728eb89
andyl <andy@r210.com>
2021-02-17 07:14:12 -0800

    First commit

---
+++ lib/xt_stack.ex
@@ -0,0 +1,18 @@
+defmodule XtStack do
+  @moduledoc """
+  Documentation for `XtStack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XtStack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xt_stack_test.exs
@@ -0,0 +1,8 @@
+defmodule XtStackTest do
+  use ExUnit.Case
+  doctest XtStack
+
+  test "greets the world" do
+    assert XtStack.hello() == :world
+  end
+end
